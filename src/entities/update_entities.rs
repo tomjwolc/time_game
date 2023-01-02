328331975b9b735edee430f7eb68ee0e1280b238
Tom Wolcott <tomjwolc@gmail.com>
2023-01-02 08:19:38 -0500

    working of grid resource

---
+++ src/entities/update_entities.rs
@@ -0,0 +1,16 @@
+use super::super::*;
+
+pub fn update_to_grid(
+    mut entities_query: Query<(&mut Transform, &mut GridCoords, &GridEntityInfo)>,
+    grid: Res<Grid>
+) {
+    for (transform, coords, grid_entity_info) in entities_query.iter_mut() {
+        let grid_entity = grid
+            .get_entity(grid_entity_info.variant, grid_entity_info.id)
+            .expect(format!("Could not find the entity: {:?}", grid_entity_info).as_str());
+        
+        
+
+
+    };
+}
